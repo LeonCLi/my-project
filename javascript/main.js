56bd827a810e965f0170221df446e405ad1933b7
Shaka Zulu <leonliewb@gmail.com>
2015-01-17 15:29:28 -0800

    added css javascript and index.html

---
+++ javascript/main.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+
+  $(document).ready(function(){
+
+    $("btn").click(function(){
+
+      $("#Calcs").val($("#Calcs").val() + $(this).html());
+
+    function $(id){return document.getElementById(id)}
+
+      function add()
+        {
+            var num1, num2, result;
+
+              num1 = number(document.one.value);
+              num2 = number(document.two.value);
+              result = num1 + num2;
+        }
+
+                function subtract()
+                  {
+                  var num1, num2, result;
+
+                num1 = number(document.one.value);
+                num2 = number(document.two.value);
+                  result = num1 + num2;
+                }
+</script>
+
+})
