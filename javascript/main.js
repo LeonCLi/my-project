5c8aa3fa0b012aadf1c12f338a9bfc29e7d15176
Shaka Zulu <leonliewb@gmail.com>
2015-01-18 16:11:15 -0800

    main.js and html

--- javascript/main.js
+++ javascript/main.js
@@ -2,12 +2,18 @@
 
   $(document).ready(function(){
 
-    var num1 = document.getElementById("X");
-    var num2 = document.getElementById("Y");
+  /*  var num1 = document.getElementById("X");
+    var num2 = document.getElementById("Y"); */
+
+    function setZ(sum) {
+      document.getElementById('sumZ').textContent = sum;
+    }
 
     $(".button").click(function(){
 
       $(".result").parseInt($('input[name="one"]').val() + $('input[name="two"]').val());
+
+    var sum = $(".result");
 
     });
 
